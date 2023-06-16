1220da23631c7bd91630e58a2fc39a6cf5d63f5b
Jotapê <106195483+JotapeJorge@users.noreply.github.com>
2023-06-16 12:30:19 -0300

    Update fetchtextbook.js

--- fetchtextbook.js
+++ fetchtextbook.js
@@ -1,4 +1,4 @@
-fetch('bio.txt')
+fetch('"https://github.com/JotapeJorge/jotape/raw/main/bio.txt')
   .then(response => response.text())
   .then(htmlText => {
     // Display the HTML content on your web page
