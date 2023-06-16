bf102a61946a9dd3e791371de2ada7fe8ccee81a
Jotapê <106195483+JotapeJorge@users.noreply.github.com>
2023-06-16 12:34:45 -0300

    Delete fetchtextbook.js

--- fetchtextbook.js
+++
@@ -1,6 +0,0 @@
-fetch('"https://github.com/JotapeJorge/jotape/raw/main/bio.txt')
-  .then(response => response.text())
-  .then(htmlText => {
-    // Display the HTML content on your web page
-    document.getElementById('bio').innerHTML = htmlText;
-  });
