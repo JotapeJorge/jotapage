b629d6cef5182b52724e31d456c80783fd71ce8c
Jotapê <106195483+JotapeJorge@users.noreply.github.com>
2023-07-28 17:48:52 -0300

    Making the site workable

---
+++ datasheets.js
@@ -0,0 +1,46 @@
+// Perform AJAX request to fetch the JSON data
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'elo/dataELOranking.json', true);
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        var jsonData = JSON.parse(xhr.responseText);
+        populateGrid(jsonData); // Call a function to populate the grid with the JSON data
+      }
+    };
+    xhr.send();
+    
+// Function to populate the SlickGrid with the JSON data
+function populateGrid(data) {
+  var columns = [
+    { id: 'Rank', name: '#', field: 'Rank', minWidth: 10, maxWidth: 50, visible: true },
+    { id: 'Time', name: 'Time', field: 'Time', visible: true },
+    { id: 'Elo', name: 'Elo', field: 'Elo', visible: true },
+    { id: '#', name: 'Rank', field: '#', minWidth: 10, maxWidth: 50, cssClass: 'hide-sm', visible: true },
+    { id: 'Média', name: 'Média', field: 'Média', cssClass: 'hide-sm', visible: true },
+    { id: 'Jogos', name: 'Jogos', field: 'Jogos', cssClass: 'hide-sm', visible: true },
+    { id: 'Vitórias', name: 'Vitórias', field: 'Vitórias', cssClass: 'hide-sm', visible: true },
+    { id: 'Derrotas', name: 'Derrotas', field: 'Derrotas', cssClass: 'hide-sm', visible: true },
+    { id: 'Gols', name: 'Gols', field: 'Gols', cssClass: 'hide-sm', visible: true },
+  ];
+
+  var options = {
+    enableCellNavigation: true,
+    enableColumnReorder: false,
+    forceFitColumns: true,
+  };
+
+  var grid = new Slick.Grid('#myGrid', data, columns, options);
+
+  // Function to handle column visibility based on screen size
+  function updateColumnVisibility() {
+    var hideColumns = window.innerWidth <= 768;
+    var visibleColumns = columns.filter((col) => !hideColumns || !col.cssClass || !col.cssClass.includes('hide-sm'));
+    grid.setColumns(visibleColumns);
+    grid.invalidate();
+    grid.render();
+  }
+
+  // Call the function initially and add a listener for future changes
+  updateColumnVisibility();
+  window.addEventListener('resize', updateColumnVisibility);
+}
---
+++ timestamp.js
@@ -0,0 +1,37 @@
+// Function to fetch the timestamp data
+async function fetchTimestampData() {
+    try {
+      const response = await fetch('elo/timestamps.json');
+      const data = await response.json();
+
+      // Access the timestamp from the JSON data
+      const timestamp = data.Timestamp['0'];
+
+      // Format the timestamp
+      const formattedTimestamp = formatTimestamp(timestamp);
+
+      // Display the formatted timestamp on the webpage
+      const timestampContainer = document.getElementById('timestampContainer');
+      timestampContainer.textContent = formattedTimestamp;
+    } catch (error) {
+      console.error('Error fetching timestamp data:', error);
+    }
+  }
+
+  // Function to format the timestamp as "Atualizado no dia d às h:m"
+  function formatTimestamp(timestamp) {
+    const date = new Date(timestamp);
+    const day = date.getDate().toString().padStart(2, '0');
+    const monthNames = [
+        'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Março', 'Junho',
+        'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+      ];
+    const month = monthNames[date.getMonth()];
+    const year = date.getFullYear();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `Atualizado no dia ${day} de ${month} de ${year} às ${hours}h${minutes}`;
+  }
+
+  // Call the function to fetch and display the timestamp data
+  fetchTimestampData();
